d0c7edac1070cc59c907a16ec7e74d6743c930cd
Dmitry <dimk00z@gmail.com>
2020-08-09 22:12:32 +0300

    Update insta_comment_parser.py

--- insta_comment_parser.py
+++ insta_comment_parser.py
@@ -6,19 +6,22 @@ from instaloader import Instaloader, Post
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Instgram comment parser')
-    parser.add_argument('--post', type=str, required=True,
+    parser.add_argument('-p', '--post', type=str, required=True,
                         help="post id")
-    parser.add_argument('--skip_author', type=bool, default=True,
+    parser.add_argument('-s', '--skip_author', type=bool, default=True,
                         help="get image directory, delault='test_photos'")
-    parser.add_argument('--xlsx_file_name', type=str, default='comments.xlsx',
+    parser.add_argument('-x', '--xlsx_file_name', type=str, default='comments.xlsx',
                         help="Excel file name")
-    parser.add_argument('--enable_debug', type=bool, default=True,
+    parser.add_argument('-d', '--enable_debug', type=bool, default=True,
                         help="Logging enable")
+    parser.add_argument('-t', '--comment_text', type=str, default='',
+                        help="Comment text")
     args = parser.parse_args()
-    return args.post, args.skip_author, args.xlsx_file_name, args.enable_debug
+    return args.post, args.skip_author, args.xlsx_file_name, \
+        args.enable_debug, args.comment_text
 
 
-def get_comments(post, skip_author):
+def get_comments(post, skip_author, comment_text):
     L = Instaloader()
     post = Post.from_shortcode(L.context, post)
     owner_username = post.owner_username
@@ -26,6 +29,13 @@ def get_comments(post, skip_author):
     for comment in post.get_comments():
         if skip_author and owner_username == comment.owner.username:
             continue
+        if comment_text:
+            next_word = False
+            for word in comment_text.split(','):
+                if word not in comment.text:
+                    next_word = True
+            if next_word:
+                continue
         comments.append({
             'username': comment.owner.username,
             'created_at': str(comment.created_at_utc),
@@ -54,10 +64,11 @@ def write_comments_to_xlsx(xlsx_file_name, comments):
 
 
 def main():
-    post, skip_author, xlsx_file_name, enable_debug = parse_args()
+    post, skip_author, xlsx_file_name, \
+        enable_debug, comment_text = parse_args()
     if enable_debug:
         logging.basicConfig(level=logging.DEBUG)
-    comments = get_comments(post, skip_author)
+    comments = get_comments(post, skip_author, comment_text)
     write_comments_to_xlsx(xlsx_file_name, comments)
 
 
